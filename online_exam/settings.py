59e5523e209fe3b9490c8ed37e209c4c09c0fec8
easywe <rmanjuriyamanjuriya@gmail.com>
2026-03-01 15:40:46 +0530

    postgres connected

--- online_exam/settings.py
+++ online_exam/settings.py
@@ -77,14 +77,20 @@ WSGI_APPLICATION = 'online_exam.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
+"""DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+"""
 
+import os
+import dj_database_url
 
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+}
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
