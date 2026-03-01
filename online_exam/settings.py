ec36eebb17ca7062a05f298501343cbb96df621c
easywe <rmanjuriyamanjuriya@gmail.com>
2026-03-01 16:38:08 +0530

    disable smtp in production

--- online_exam/settings.py
+++ online_exam/settings.py
@@ -147,9 +147,11 @@ SESSION_EXPIRE_AT_BROWSER_CLOSE = True
 
 
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+"""EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'csprogramer19@gmail.com'
-EMAIL_HOST_PASSWORD = 'iaff wzoh fivh vgil'
+EMAIL_HOST_PASSWORD = 'iaff wzoh fivh vgil'"""
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
